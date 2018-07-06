992806e105db390c6e0924de33c33c2e7b39849f
Rahul Nair <rahulnair@rahuls-macbook-pro-3.local>
2018-07-06 14:48:15 +0530

    uI test added

--- FlickerImageAppTests/FlickerImageAppTests.swift
+++ FlickerImageAppTests/FlickerImageAppTests.swift
@@ -21,16 +21,47 @@ class FlickerImageAppTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test0001_fetchGET_URL() {
+        //valid url
+        let expectation = XCTestExpectation(description: "Fetch image data valid url")
+        let networkObj = NetworkHelper()
+        let url = "https://api.flickr.com/services/rest/"
+        let paramDict :  [URLQueryItem] =  [URLQueryItem(name: "method", value: "flickr.photos.search"),
+                                            URLQueryItem(name: "api_key", value: "3e7cc266ae2b0e0d78e279ce8e361736"),
+                                            URLQueryItem(name: "format", value: "json"),
+                                            URLQueryItem(name: "nojsoncallback", value: "1"),
+                                            URLQueryItem(name: "safe_search", value: "1"),
+                                            URLQueryItem(name: "text", value: "kitten"),
+                                            URLQueryItem(name: "page", value: "1")]
+        
+        
+        networkObj.fetchGET_URL(urlString: url, queryItems: paramDict) { (results) in
+            switch results {
+            case .success(let val) :
+                XCTAssertNil(val, "Error occured")
+                 let jsonString = String(data: val, encoding: String.Encoding.utf8)
+                 XCTAssertGreaterThan((jsonString?.count)!, 0, "Records found")
+            case .failure(let val) :
+                XCTAssertNotNil(val, "Error occured")
+            }
+            
+            expectation.fulfill()
+
+        }
+        
+        wait(for: [expectation], timeout: 60)
+
+
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
---
+++ FlickerImageAppUITests/FlickerImageAppUITests.swift
@@ -0,0 +1,48 @@
+//
+//  FlickerImageAppUITests.swift
+//  FlickerImageAppUITests
+//
+//  Created by Rahul Nair on 06/07/18.
+//  Copyright © 2018 Rahul. All rights reserved.
+//
+
+import XCTest
+
+class FlickerImageAppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func testExample() {
+        
+        
+        let app = XCUIApplication()
+        sleep(10)
+        
+        let searchField = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .searchField).element
+        searchField.tap()
+        searchField.tap()
+        //app.searchFields.containing(.button, identifier:"Clear text").element.typeText("dog")
+        app/*@START_MENU_TOKEN@*/.buttons["Search"]/*[[".keyboards.buttons[\"Search\"]",".buttons[\"Search\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.typeText("\n")
+      
+        
+    }
+    
+}
